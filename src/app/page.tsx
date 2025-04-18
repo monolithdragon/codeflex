faea533e6ed4f40aa5f4f28904bc847fc59580c0
monolithdragon <joo.lajos76@gmail.com>
2025-04-18 09:07:47 +0200

    Create Clerk Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+'use client';
+
+import { SignedIn, SignedOut, SignInButton } from '@clerk/clerk-react';
+import { SignOutButton } from '@clerk/nextjs';
 
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			HomePage
+			<SignedIn>
+				<SignOutButton />
+			</SignedIn>
+			<SignedOut>
+				<SignInButton />
+			</SignedOut>
+		</div>
+	);
 };
 
 export default HomePage;
